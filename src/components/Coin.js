96d49499141312d35dbb84d98136f36a8e8efb88
KieranSOC <kieranSOC@Kierans-MacBook-Air.local>
2022-02-21 02:01:13 +1100

    fixed spacing issue

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -3,10 +3,10 @@ import React from "react";
 function Coin({ name, icon, price, symbol }) {
   return (
     <div className="coin">
-      <h1>Name:{name} </h1>
-      <img src={icon} />
-      <h3>Price:${(Math.round(price * 100) / 100).toFixed(2)}</h3>
-      <h3>Symbol:{symbol}</h3>
+      <h1>Name: {name} </h1>
+      <img src={icon} alt="" />
+      <h3>Price: ${(Math.round(price * 100) / 100).toFixed(2)}</h3>
+      <h3>Symbol: {symbol}</h3>
     </div>
   );
 }
