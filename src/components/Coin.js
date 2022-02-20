31da00bd14e7c54338f380a96c27231df5288681
KieranSOC <kieranSOC@Kierans-MacBook-Air.local>
2022-02-21 02:02:07 +1100

    removed name placeholder

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -3,7 +3,7 @@ import React from "react";
 function Coin({ name, icon, price, symbol }) {
   return (
     <div className="coin">
-      <h1>Name: {name} </h1>
+      <h1>{name}</h1>
       <img src={icon} alt="" />
       <h3>Price: ${(Math.round(price * 100) / 100).toFixed(2)}</h3>
       <h3>Symbol: {symbol}</h3>
