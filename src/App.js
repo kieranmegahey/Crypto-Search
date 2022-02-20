dc75c24c954487aea0c7e8ef65dc4505b1b19b4e
KieranSOC <kieranSOC@Kierans-MacBook-Air.local>
2022-02-21 01:31:28 +1100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Axios from "axios";
+import Coin from "./components/Coin";
 
 function App() {
+  const [coinData, setCoinData] = useState([]);
+  const [search, setSearch] = useState("");
+  useEffect(() => {
+    Axios.get("https://api.coinstats.app/public/v1/coins?skip=0").then(
+      (response) => {
+        setCoinData(response.data.coins);
+      }
+    );
+  }, []);
+
+  const filteredCoins = coinData.filter((coin) => {
+    return coin.name.toLowerCase().includes(search.toLowerCase());
+  });
+
+  function handleOnChange(event) {
+    // event.preventDefault();
+    const searchWord = event.target.value;
+
+    return setSearch(searchWord);
+  }
+  console.log(search);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="cryptoHeader">
+        <input onChange={handleOnChange} type="text" />
+      </div>
+
+      <div className="cryptoDisplay">
+        {filteredCoins.map((coin) => {
+          return (
+            <Coin
+              key={coin.id}
+              name={coin.name}
+              icon={coin.icon}
+              price={coin.price}
+              symbol={coin.symbol}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
